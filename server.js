6bea32586774f99b12deb4af8982c9de05f97d8e
kaisei-2026 <sota3428@gmail.com>
2026-05-28 21:58:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io")(http, {
-  cors: { origin: "*" }
-});
+const io = require("socket.io")(http, { cors: { origin: "*" } });
 
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  socket.on("sync", (t) => socket.emit("sync_res", { c: t, s: Date.now() }));
-  socket.on("start_signal", (data) => io.emit("start_broadcast", data));
+  // 部屋に参加する
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+    console.log(`User joined room: ${roomId}`);
+  });
+
+  // その部屋だけに時刻同期を返す
+  socket.on("sync", (data) => {
+    socket.emit("sync_res", { c: data.c, s: Date.now() });
+  });
+
+  // その部屋だけにスタート信号を送る
+  socket.on("start_signal", (data) => {
+    io.to(data.roomId).emit("start_broadcast", data);
+  });
+
+  // その部屋だけにストップ信号を送る
+  socket.on("stop_signal", (data) => {
+    io.to(data.roomId).emit("stop_broadcast", data);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
